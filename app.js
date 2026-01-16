36b3fae45b692613fec0693451dad53a5c6cbef2
Instamod88 <132873133+Instamod88@users.noreply.github.com>
2026-01-16 08:06:13 -0300

    Criar app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+function gerarSensi() {
+  const dpi = document.getElementById("dpi").value;
+  const puxada = document.getElementById("puxada").value;
+  const problema = document.getElementById("problema").value;
+
+  const sensi = calcularSensi(dpi, puxada, problema);
+
+  document.getElementById("resultado").innerHTML = `
+    <h2>Sensibilidade Ideal</h2>
+    Geral: ${sensi.geral}<br>
+    Red Dot: ${sensi.redDot}<br>
+    2x: ${sensi.mira2x}<br>
+    4x: ${sensi.mira4x}<br>
+    AWM: ${sensi.awm}
+  `;
+}
